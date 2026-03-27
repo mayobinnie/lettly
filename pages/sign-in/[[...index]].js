71835deb73d3ce2f8b007e8416dd61dbfe81227c
mayobinnie <mayo.binnie@gmail.com>
2026-03-27 17:34:54 +0000

    Delete pages directory

--- pages/sign-in/[[...index]].js
+++
@@ -1,6 +0,0 @@
-import { SignIn } from '@clerk/nextjs'
-export default function SignInPage(){
-  return <div style={{minHeight:'100vh',background:'var(--bg)',display:'flex',alignItems:'center',justifyContent:'center',padding:20}}>
-    <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" afterSignInUrl="/dashboard"/>
-  </div>
-}
